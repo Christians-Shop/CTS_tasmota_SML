785848904c79394f8402f53eabadf0ea447b874a
Christians-Shop <cschepelmann@christians-shop.de>
2023-09-25 11:57:45 +0200

    Update user_config_override.h

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -87,8 +87,14 @@ Examples :
 // !!! Remember that your changes GOES AT THE BOTTOM OF THIS FILE right before the last #endif !!!
 */
 
-
-
-
+#ifndef USE_SCRIPT
+#define USE_SCRIPT
+#endif
+#ifndef USE_SML_M
+#define USE_SML_M
+#endif
+#ifdef USE_RULES
+#undef USE_RULES
+#endif
 
 #endif  // _USER_CONFIG_OVERRIDE_H_
