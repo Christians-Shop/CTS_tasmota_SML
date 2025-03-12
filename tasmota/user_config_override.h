562b5726b867fece677bf9f09cdcc55deba5bb87
cschepelmann <cschepelmann@christians-shop.de>
2025-03-13 00:18:23 +0100

    Update für ESP32

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -97,4 +97,8 @@ Examples :
 #undef USE_RULES
 #endif
 
+#define START_SCRIPT_FROM_BOOT
+#define USER_BACKLOG "script 1"
+#define PRECONFIGURED_SCRIPT ">D\n>B\n=>sensor53 r\n>M 1\n+1,3,s,0,9600,SMI,1\n1,77070100010800ff@1000,Total Consumed,kWh,Total_in,2\n1,77070100020800ff@1000,Total Delivered,kWh,Total_out,2\n1,77070100100700ff@1,Power,W,Power_cur,0\n#\n"
+
 #endif  // _USER_CONFIG_OVERRIDE_H_
